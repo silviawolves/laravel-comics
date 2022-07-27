cb41ec8fb726d2ef17622887f96440cf7909b396
silviawolves <silviacanepa.w@gmail.com>
2022-07-27 18:17:24 +0200

    esercizio vagamente lungo

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -8,6 +8,7 @@
     <div class="blue-div"></div>
     <div class="container pt-5 pb-3 position-relative">
 
+        {{-- IMMAGINE FUMETTO --}}
         <img src="{{ $fumetto['thumb'] }}" class="fumetto-show">
         <div class="fumetto-banner top">Comic book</div>
         <a href="{{ route('home') }}"><div class="fumetto-banner low">View gallery</div></a>
@@ -17,26 +18,26 @@
                 <div class="col-8">
                     <h3 class="text-uppercase">{{ $fumetto['title'] }}</h3>
                     
+                    {{-- BANNER VERDE --}}
                     <div class="d-flex py-3">
                         <div class="green-banner large">
-                            <div>
-                                <span class="greeny">U.S. Price:</span> {{ $fumetto['price'] }}
-                            </div>
-                            <div class="greeny text-uppercase">
-                                Available
-                            </div>
-                        
+                            <div><span class="greeny">U.S. Price:</span> {{ $fumetto['price'] }}</div>
+                            <div class="greeny text-uppercase">Available</div>
                         </div>
+
                         <div class="green-banner small">Check Availability</div>
                     </div>
 
+                    {{-- DESCRIZIONE FUMETTO --}}
                     <p>{!! $fumetto['description'] !!}</p>
-
-
                 </div>
 
+                {{-- ADVERTISING A LATO --}}
                 <div class="col-4">
-
+                    <div class="content">
+                        <h6 class="adv">Advertisement</h6>
+                        <img src="img/adv.jpg" alt="">
+                    </div>
                 </div>
 
         </div>
